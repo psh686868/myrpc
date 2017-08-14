a3ba8ebc203e5bd752ddd94bacf0c6023c3bc56f
psh686868 <1101311464@qq.com>
2017-08-14 20:12:59 +0800

    提交项目

---
+++ src/main/java/com/psh/framework/ConsumerProxy.java
@@ -0,0 +1,7 @@
+package com.psh.framework;
+
+/**
+ * Created by shitou on 17-8-14.
+ */
+public class ConsumerProxy {
+}
---
+++ src/main/java/com/psh/service/HelloService.java
@@ -0,0 +1,7 @@
+package com.psh.service;
+
+/**
+ * Created by shitou on 17-8-14.
+ */
+public class HelloService {
+}
