e978b0bd67d71307692b23f9a512dbb1a3a52d8b
psh686868 <1101311464@qq.com>
2017-08-14 20:45:43 +0800

    添加rpc

---
+++ src/main/java/com/psh/framework/ProviderReflect.java
@@ -0,0 +1,8 @@
+package com.psh.framework;
+
+/**
+ * Created by shitou on 17-8-14.
+ */
+public class ProviderReflect {
+
+}
---
+++ src/main/java/com/psh/invoke/RpcProviderMain.java
@@ -0,0 +1,7 @@
+package com.psh.invoke;
+
+/**
+ * Created by shitou on 17-8-14.
+ */
+public class RpcProviderMain {
+}
