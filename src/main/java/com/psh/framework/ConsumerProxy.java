5822ef76a86d336e68b0324e91e416a6426f2776
psh686868 <1101311464@qq.com>
2017-08-16 19:06:35 +0800

    添加远程方法调用

--- src/main/java/com/psh/framework/ConsumerProxy.java
+++ src/main/java/com/psh/framework/ConsumerProxy.java
@@ -1,12 +1,54 @@
 package com.psh.framework;
 
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Proxy;
+import java.net.Socket;
+
 /**
  * Created by shitou on 17-8-14.
  * 服务消费代理接口，因为是代理接口，1.  泛型代替工厂 2 选择通信机制为scoket
  */
 public class ConsumerProxy {
-    public static <T>T consume(final Class<T> tClass,final String ip,final String port){
-        //通过scoket传入接口对象，
-        return null;
+    public static <T>T consume(final Class<T> tClass,final String ip,final int port){
+        //通过scoket传入接口对象，方法名和参数，返回远程端返回的值
+        return (T)Proxy.newProxyInstance(tClass.getClassLoader(),tClass.getInterfaces(),(proxy,methods,arguments)->{
+            //方法执行器
+            Socket socket = null;
+            InputStream inputStream = null;
+            OutputStream outputStream = null;
+            try {
+                new Socket(ip,port);
+                inputStream = socket.getInputStream();
+
+                //写入方法名
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+                objectOutputStream.writeUTF(methods.getName());
+                //写入方法参数
+                objectOutputStream.writeObject(arguments);
+
+                //处理 远程调用返回的结果
+                ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+                Object result = objectInputStream.readObject();
+                if(result instanceof Throwable){
+                    throw (Throwable) result;
+                }
+                return result;
+            }finally {
+                if(inputStream!=null){
+                    inputStream.close();
+                }
+                if(outputStream!=null){
+                    outputStream.close();
+                }
+                if(socket!=null){
+                    socket.close();
+                }
+            }
+
+        });
+
     }
 }
--- src/main/java/com/psh/framework/ProviderReflect.java
+++ src/main/java/com/psh/framework/ProviderReflect.java
@@ -1,11 +1,28 @@
 package com.psh.framework;
 
+import org.apache.commons.lang3.reflect.MethodUtils;
+
+
+
+import java.io.*;
+import java.lang.reflect.InvocationTargetException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Created by shitou on 17-8-14.
  */
 public class ProviderReflect {
     private static final int initPort = 8000;
-    public static <T>T provider(Class service,int port){
+
+    //来个懒加载进行进行初始化线程池
+    private static class ExecutorServiceProd{
+        private static ExecutorService executorService = Executors.newCachedThreadPool();
+    }
+
+    public static void provider(final Object service, int port){
         if(service==null){
             throw new RuntimeException("服务接口未传参");
         }
@@ -15,9 +32,70 @@ public class ProviderReflect {
             port = ProviderReflect.initPort;
         }
 
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            ExecutorService executorService = ExecutorServiceProd.executorService;
+
+            while (true){
+                final Socket socket = serverSocket.accept();
+
+                executorService.execute(() -> {
+                    ObjectInputStream objectInputStream = null;
+                    ObjectOutputStream objectOutputStream = null;
+                    try {
+                        //获取对象流
+                        final InputStream inputStream = socket.getInputStream();
+                        final OutputStream outputStream = socket.getOutputStream();
+                        objectInputStream = new ObjectInputStream(inputStream);
+                        //获取方法
+                        String method = objectInputStream.readUTF();
+                        //获取方法参数
+                        ObjectInputStream objectInputStreamArgs = new ObjectInputStream(inputStream);
+                        Object args = objectInputStreamArgs.readObject();
+
+                        //执行方法  三个参数 对象target  ，方法名， 参数列表
+
+                        Object result = MethodUtils.invokeExactMethod(service, method, args);
+
+                        //将方法调用返回值返回
+                        objectOutputStream = new ObjectOutputStream(outputStream);
+                        objectOutputStream.writeObject(result);
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }catch (ClassNotFoundException e){
+                        e.printStackTrace();
+                    } catch (NoSuchMethodException e) {
+                        e.printStackTrace();
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } catch (InvocationTargetException e) {
+                        e.printStackTrace();
+                    }finally {
+                        if(objectOutputStream!=null){
+                            try {
+                                objectOutputStream.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        if(objectInputStream!=null){
+                            try {
+                                objectInputStream.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
-        return null;
+        return ;
     }
 
 
